b1fe2547d1aaa2a8788c55429aeb38ae93886bbb
dawnniie <lachlan.walls1@gmail.com>
2024-04-30 21:30:23 +1000

    fix: improve test

--- src/test.ts
+++ src/test.ts
@@ -1,21 +1,29 @@
 import { attachConfigurations, configure } from './index.js'
 
-interface BroadStructure {
+interface SillyPost {
   id: string
-  str: string
-  num: number
-  list: string[]
+  name: string
+  forum_id: string
+  from_user_id: string
+  to_user_id: string
+  tags: string[]
+  likes: number
 }
 
 const tableConfig = {
-  test: configure<BroadStructure>('rdb-ts-extra-test', 'data')({
-    str: {},
-    list: { multi: true },
-    compound: { compound: ['id', 'num', 'list'] },
-    custom_multi: { custom: row => [row('id'), row('str')], multi: true },
-    custom_multi_compound: { custom: row => [[row('id'), row('str')], [row('id'), row('num')]] as const, multi: true },
-    custom: { custom: row => row('num').add(1) },
-    custom_compound: { custom: row => [row('id'), row('str')] as const }
+  test: configure<SillyPost>('rdb-ts-extra-test', 'data')({
+    forum_id: {},
+    tags: { multi: true },
+    // compound index allowing to order posts in a 'forum' by like count
+    forum_search: { compound: ['forum_id', 'likes'] },
+    // custom multi index allowing two fields to be used in one index, in this case to retrieve from either user's perspective
+    user_id: { custom: row => [row('from_user_id'), row('to_user_id')], multi: true },
+    // custom multi compound index allowing multiple compound indexes to be assigned, effectively a combination of the above two indexes
+    user_search: { custom: row => [[row('from_user_id'), row('likes')], [row('to_user_id'), row('likes')]] as const, multi: true },
+    // custom index, in this case a kind of 'quality' based on the number of tags and likes
+    quality: { custom: row => row('tags').count().mul(3).add(row('likes').count()) },
+    // custom compound index, when a compound index needs to be more complex
+    identifier: { custom: row => [row('forum_id'), row('name').downcase()] as const }
   })
 }
 
@@ -24,13 +32,12 @@ const { r, upgrade } = attachConfigurations(tableConfig)
 await r.connectPool()
 await upgrade({ db: 'rdb-ts-extra-test' })
 
-await r.test.getAll('test', { index: 'str' }).run()
-await r.test.getAll('test', { index: 'list' }).run()
-await r.test.getAll(['test', 3, ['hello']], { index: 'compound' }).run()
-await r.test.getAll('test', { index: 'custom_multi' }).run()
-await r.test.getAll(['test', 'hello'], { index: 'custom_multi_compound' }).run()
-await r.test.getAll(['test', 2], { index: 'custom_multi_compound' }).run()
-await r.test.getAll(4, { index: 'custom' }).run()
-await r.test.getAll(['test', 'hello'], { index: 'custom_compound' }).run()
+await r.test.getAll('test', { index: 'forum_id' }).run()
+await r.test.getAll('tag', { index: 'tags' }).run()
+await r.test.between(['test', r.minval], ['test', r.maxval], { index: 'forum_search' }).orderBy({ index: 'forum_search' }).run()
+await r.test.getAll('test', { index: 'user_id' }).run()
+await r.test.between(['test', r.minval], ['test', r.maxval], { index: 'user_search' }).orderBy({ index: 'user_search' }).run()
+await r.test.between(10, r.maxval, { index: 'quality' }).orderBy({ index: 'quality' }).run()
+await r.test.getAll(['test', 'hello'], { index: 'identifier' }).run()
 
 await r.getPoolMaster()?.drain()
